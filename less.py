fd6f3d7a0682e435292dd634468ee7addd7f0124
unknown <marselmaho>
2023-04-04 15:43:44 +0600

    first commit

---
+++ less.py
@@ -0,0 +1,49 @@
+# list.append(x)	Добавляет элемент в конец списка
+# list.extend(L)	Расширяет список list, добавляя в конец все элементы списка L
+# list.insert(i, x)	Вставляет на i-ый элемент значение x
+# list.remove(x)	Удаляет первый элемент в списке, имеющий значение x. ValueError, если такого элемента не существует
+# list.pop([i])	Удаляет i-ый элемент и возвращает его. Если индекс не указан, удаляется последний элемент
+# list.index(x, [start [, end]])	Возвращает положение первого элемента со значением x (при этом поиск ведется от start до end)
+# list.count(x)	Возвращает количество элементов со значением x
+# list.sort([key=функция])	Сортирует список на основе функции
+# list.reverse()	Разворачивает список
+# list.copy()	Поверхностная копия списка
+# list.clear()	Очищает список
+
+# lst = [1,2,3]
+# lst.extend([4,5,6])
+# print(lst)
+
+# hv=[i for i in 'yasin']
+# print(''.join(hv))
+
+# n1 = ['appl', 'banan', 'chty']
+# n1.insert(3,'pert')
+# print(n1)
+
+# n2=['python', 'java']
+# n3=['sdsdsd','sdsdSDh']
+# print(n2+[n3])
+
+# geyms= ['dota ', 'pabg ']
+# geyms.extend(['fry faer','minicraft','mabaillejens'])
+# print(geyms)
+
+# Python_group = ['Marsel','Aman','Akyl','Saule','Nursalam']
+# Python_group.remove('Nursalam')
+# print(Python_group)
+
+# tele_lst = ['Iphone','Samsung','Nokia','Banan']
+# A=tele_lst.pop(3)
+
+# print(A)
+
+# numbers_lst = [1,3,4,5,6,7,8,9,10]
+# numbers_lst.insert(1, 2)
+# print(numbers_lst)
+
+aaa=[112,2,222,2,2,2,2,334,7777]
+sss=max(aaa)
+print(sss)
+
+
